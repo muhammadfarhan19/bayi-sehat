ab8a129e974459d4d1196bc01ea66352d5ab3486
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2024-01-15 22:57:31 +0700

    fix:var name

--- src/components/SideLoginPage.tsx
+++ src/components/SideLoginPage.tsx
@@ -2,7 +2,7 @@ import ImageContainer from "./customComponents/ImageContainer";
 
 const SideLoginPage = () => (
   <aside className="h-full sm:w-full hidden sm:flex flex-col gap-10 justify-center items-center">
-    <ImageContainer customClassName="sm:w-3/4 md:w-1/2 lg:w-2/5" />
+    <ImageContainer style="sm:w-3/4 md:w-1/2 lg:w-2/5" />
     <section className="text-center text-2xl font-bold text-white ">
       <p>Sayangi Anak Kita</p>
       <p>Cegah Stunting Sejak Dini</p>
