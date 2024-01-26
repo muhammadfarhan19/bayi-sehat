d2964fd8d08e29ecee6e981b5e945e9d13b9cf18
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2024-01-26 20:12:12 +0700

    style: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import Hamburger from "./customComponents/Hamburger";
 
 const Navbar = () => {
   return (
-    <nav className="h-14 sm:h-20 w-full flex justify-between items-center py-2 px-5 sm:py-5 sm:px-9 border-b border-teal-500">
+    <nav className="h-14 sm:h-20 w-full flex justify-between items-center py-2 px-5 sm:py-5 sm:px-9 bg-teal-500">
       <Hamburger />
       <Link
         href="/dashboard"
-        className="font-bold text-xl sm:text-2xl p-0 text-teal-500"
+        className="font-bold text-xl sm:text-2xl p-0 text-white"
       >
         bayisehat.com
       </Link>
